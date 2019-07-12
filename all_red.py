4aef11934f7a11da7c84d548ea7ed42f1dbbad98
DrTexxOfficial <23412100+DrTexxOfficial@users.noreply.github.com>
2019-07-12 23:14:16 +1000

    added simple user scripts

---
+++ all_red.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+import os
+os.system("python3 broadcast_color.py red")
---
+++ default_lighting.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+import os
+os.system("python3 broadcast_color.py 1456 0 33968 3500")

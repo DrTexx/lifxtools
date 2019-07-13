e40e67b60822f0084e09a87576d58cc57cf29f2b
DrTexxOfficial <23412100+DrTexxOfficial@users.noreply.github.com>
2019-07-13 11:20:23 +1000

    live data viewing added, begin making iterable

--- gui.py
+++ gui.py
@@ -11,6 +11,7 @@ from lifxlan import LifxLAN, RED, WHITE # used for controlling lights
 num_lights = None
 default_color = (58275, 0, 33968, 3500) # roughly 50% brightness at 3500k
 bedtime_color = (58275, 0, 18010, 2000) # roughly 25% brightness at 2000k
+live_data = True
 debug = True
 
 # configuration
@@ -61,6 +62,9 @@ def set_light_color(_light,color):
     _light.set_color(color)
     if (debug==True): print("{} color set to {}".format(_light.get_label(),color))
 
+def get_light_color(_light): return(_light.get_color())
+def get_light_brightness(_light): return(get_light_color(_light)[2])
+
 # main function
 def main():
     lifx = return_interface()
@@ -85,18 +89,39 @@ def main():
             test_frame = Frame(m)
             test_frame.pack()
 
+            live_data_frame = Frame(test_frame)
+            live_data_frame.pack()
+
+            live_data_checkbox = Checkbutton(live_data_frame, text="Enable live data", var=live_data)
+            live_data_checkbox.var = live_data
+            live_data_checkbox.pack()
+
             test_text = Label(test_frame,text="Lights discovered")
             test_text.pack()
 
-            light0_frame = Frame(test_frame)
-            light0_frame.config(padding=5,relief=SUNKEN)
-            light0_frame.pack()
+            self.light_settings = []
+            i = 0
+            for device in devices:
+                self.light_settings.append({'device': device, 'frame': Frame(test_frame)}) # add frames and all relevant things here
+                # configure frames and relevant items here
+                self.light_settings[i]['frame'].config(padding=5,relief=SUNKEN)
+                self.light_settings[i]['frame'].pack()
+
+            print(self.light_settings)
+            light0_frame = self.light_settings[0]['frame'] # TEMPORARY UNTIL CODE BELOW MIGRATED TO ITERABLE FORMAT
+
+            # light0_frame = Frame(test_frame)
+            # light0_frame.config(padding=5,relief=SUNKEN)
+            # light0_frame.pack()
 
             light0_name = Label(light0_frame, text=str(devices[0].get_label()))
             light0_name.pack(side=LEFT)
 
             light0_toggle = Button(light0_frame, text="toggle power", command=self.toggle_light_0)
             light0_toggle.pack(side=LEFT)
+
+            self.light0_brightness = Scale(light0_frame, from_=0, to=65535,orient=HORIZONTAL)
+            self.light0_brightness.pack(side=LEFT)
 
             light0_reset_color = Button(light0_frame, text="reset color", command=self.reset_color_light_0)
             light0_reset_color.pack(side=LEFT)
@@ -115,11 +140,18 @@ def main():
         def bedtime_color_light_0(self): set_light_color(devices[0],bedtime_color)
         def set_white_light_0(self): set_light_color(devices[0],WHITE)
         def set_red_light_0(self): set_light_color(devices[0],RED)
+        def _update_brightness(self): self.light0_brightness.set(get_light_brightness(devices[0]))
+
+        def _update_loop(self,ms_per_loop=1000):
+            if (live_data == True):
+                self._update_brightness()
+            self.after(ms_per_loop,self._update_loop) # repeat _update_loop()
 
         def _exit_app(self,event):
             exit()
 
     app = Window(root)
+    app._update_loop() # must be before main loop
     root.mainloop()
 
 if __name__=="__main__":

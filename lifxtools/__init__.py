f50bf1b44aa06b0cc6af037979631ab4546d6693
DrTexxOfficial <23412100+DrTexxOfficial@users.noreply.github.com>
2019-07-14 16:30:26 +1000

    add managedLight class

--- lifxtools/__init__.py
+++ lifxtools/__init__.py
@@ -98,3 +98,35 @@ def get_lights(_interface,debug=False):
     finally:
         if (debug == True): print("[ get lights ] finished!")
 
+class managedLight:
+
+    def __init__(self,_light,debug=False):
+        if (debug == True): print("creating a new managedLight...")
+        self.light = _light
+        self.power = None
+        self.color = None
+        self.infrared = None
+
+    def ssave(self):
+        '''save light state inside of class'''
+        self.power = self.light.get_power()
+        self.color = self.light.get_color()
+        #self.infrared = self.light.get_infrared()
+
+    def sload(self):
+        '''load light state last saved inside of class'''
+        self.light.set_power(self.power)
+        self.light.set_color(self.color)
+        #self.light.set_infrared(self.infrared)
+
+    def sexport(self):
+        '''export light state to outside class'''
+        pass
+
+    def simport(self):
+        '''import light state from outside class'''
+        pass
+
+    def print_saved_state(self):
+        '''print the saved state of the light'''
+        print("[{} (saved)] power:{} color:{} infrared:{}".format(self.light.get_label(), self.power, self.color, self.infrared))

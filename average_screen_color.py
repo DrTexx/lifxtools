d7f9ff8443a2c2d45ab5929c85957d9e5f09023d
DrTexxOfficial <23412100+DrTexxOfficial@users.noreply.github.com>
2019-07-14 22:10:51 +1000

    add option for monitor colour temperature

--- average_screen_color.py
+++ average_screen_color.py
@@ -20,6 +20,7 @@ fade_modes = {'game': 0, 'movie': 150, 'desktop': 300, 'slow': 1000, 'super-slow
 # preferences
 factor = 0.75
 fade_mode = fade_modes['slow']
+monitor_color_temp = 3000 # normal:5500, NightLightMode:3000
 
 # split image filename into name and extension
 #name, ext = os.path.splitext(image_file)
@@ -100,7 +101,7 @@ def main():
     while True:
         average_red, average_green, average_blue = scan_screen(sample_x,sample_y)
         h, s, v = rgb2hsv(average_red, average_green, average_blue)
-        color = (h, s, v, 5500)
+        color = (h, s, v, monitor_color_temp)
         for light in lights:
             light.set_color(color,fade_mode,rapid=True)
 

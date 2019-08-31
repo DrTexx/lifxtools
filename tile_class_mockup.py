04ae8a0a97600bc954b91e9e1b591e08a8d1f2ac
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-08-31 17:52:58 +1000

    tile animation example works! (wip)

--- tile_class_mockup.py
+++ tile_class_mockup.py
@@ -1,17 +1,18 @@
 import numpy as np
 import lifxlan
+from time import sleep
 
 # ---- classes ----
 
 class TileFrame:
 
     def __init__(self,x_size,y_size):
-        self._x_size = x_size
-        self._y_size = y_size
+        self.x_size = x_size
+        self.y_size = y_size
         self.frame = self._gen_empty_frame()
 
     def _gen_empty_frame(self):
-        empty_frame = np.zeros((x_size,y_size), dtype=object)
+        empty_frame = np.zeros((self.x_size,self.y_size), dtype=object)
         empty_color = (0,0,0,6500)
 
         for y in range(len(empty_frame)):
@@ -36,32 +37,44 @@ class TileFrame:
 # ---- settings (for script) ----
 
 x_size, y_size = (8,8)
-color_a = (65535,65535,65535,6500)
+color_a = (0,65535,65535,6500)
 
 # ---- script ----
 
 # instantiate a frame object
-my_frame = TileFrame(x_size,y_size)
+frame_1 = TileFrame(x_size,y_size)
+frame_2 = TileFrame(x_size,y_size)
 
-# print new empty frame object
-my_frame.print_frame()
+frame_1.print_frame() # print frame data to CLI
+frame_2.print_frame() # print frame data to CLI
 
-# set a single pixel of frame
-my_frame.set_pixel(0,0,color_a)
+# set a each pixel of frame based on these conditions
+for y in range(frame_1.y_size):
+    for x in range(frame_1.x_size):
+        if (y % 2 == 0):
+            if (x % 2 == 0):
+                frame_1.set_pixel(x,y,color_a)
 
-# print frame with set pixel
-my_frame.print_frame()
+# set a each pixel of frame based on these conditions
+for y in range(frame_2.y_size):
+    for x in range(frame_2.x_size):
+        if not (y % 2 == 0):
+            if not (x % 2 == 0):
+                frame_2.set_pixel(x,y,color_a)
 
-# change pixel by access
-my_frame.frame[0][1] = color_a
+frame_1.print_frame() # print frame data to CLI
+frame_2.print_frame() # print frame data to CLI
 
-# print frame with set pixel
-my_frame.print_frame()
+frames = [frame_1, frame_2]
 
 # ---- lifx ----
 lifx = lifxlan.LifxLAN()
 tilechains = lifx.get_tilechain_lights()
 
-for tilechain in tilechains:
-    # tilechain.set_tile_colors(start_index, colors, [duration], [tile_count], [x], [y], [width], [rapid])
-    tilechain.set_tile_colors(0, my_frame.return_frame(), 0)
+# tilechain.set_tile_colors(start_index, colors, [duration], [tile_count], [x], [y], [width], [rapid])
+
+while True:
+    for frame in frames:
+        for tilechain in tilechains:
+            tilechain.set_tile_colors(0, frame.return_frame(), 0, rapid=True)
+        sleep(0.1)
